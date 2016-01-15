0b755b917a72ca5a1f694d4b084006425a50aeaf
Lakshman <ameex@ameex.(none)>
2016-01-15 21:59:17 +0530

    updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,10 +35,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.sendgrid.net",
+      :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => "lakshman90",
-      :password             => 'SMACKTHAT2',
+      :user_name            => "lakshmanmurugesan@gmail.com",
+      :password             => 'SMACKTHAT',
       :authentication       => "plain",
       :enable_starttls_auto => true,
       :openssl_verify_mode => 'none'
