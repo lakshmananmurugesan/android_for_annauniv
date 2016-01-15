1b82e0fd16beda7bd03d8d18d4189fe3ef9f62aa
Lakshman <ameex@ameex.(none)>
2016-01-15 20:09:45 +0530

    updated admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :phone
+    devise_parameter_sanitizer.for(:account_update) << :phone
+  end
+
   def isAdmin?
     if current_user != nil
-      if current_user.email == "superuser@superuser.com"
+      if current_user.email.strip == "sivaprasathmjn@gmail.com"
         return true
       end
     end
